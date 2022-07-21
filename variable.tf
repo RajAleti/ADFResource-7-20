f3c7c65a636238de2f8699a97545b3973c4b660e
RajAleti <rajaleti111.origin@gmail.com>
2022-07-21 00:27:47 -0500

    variable file is added

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  default = "rg-test-adf-resources"
+}
+variable "location" {
+  default = "West Europe"
+}
+
+variable "name" {
+  default = "Datafactory-128-raja"
+}
